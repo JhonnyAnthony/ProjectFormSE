398f88904af9cb304d0c8e32e4da946ef733a3c9
Jhonny Anthony de Souza <125496657+JhonnyAnthony@users.noreply.github.com>
2024-09-27 12:42:31 -0300

    Init - ERROR 'AADSTS900971: No reply address provided. '

--- src/loginSelenium.py
+++ src/loginSelenium.py
@@ -1,3 +1,4 @@
+# import requests
 # import webbrowser
 # from selenium import webdriver
 # from selenium.webdriver.common.by import By
@@ -8,34 +9,35 @@
 # import keyring
 # import logging
 # from selenium.common.exceptions import WebDriverException
+# from msal import ConfidentialClientApplication, PublicClientApplication
 
-# logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 # def login_and_click():
 #     # Retrieve credentials
-#     username = "jhonny.souza@fgmdentalgroup.com"
-#     password = keyring.get_password("MicrosoftForms", username)
+#     username = ""
+#     password = ""
 #     # Set up the WebDriver (make sure you have the appropriate driver installed)
 #     driver = webdriver.Chrome()
+
 #     try:
 #         # Open the specified webpage
-#         driver.get("https://login.microsoftonline.com/e220d369-e6a8-4126-83ef-b4b6bbfd9367/")
+#         driver.get("https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=8c59ead7-d703-4a27-9e55-c96a0054c8d2")
 
 #         # Locate the username input field using XPath and send the username
-#         username_field = driver.find_element(By.XPATH, '//*[@id="userid"]')
+#         username_field = driver.find_element(By.XPATH, '//*[@id="i0116"]')
 #         username_field.send_keys(username)
 
 
 #         # Locate the password input field using XPath and send the password
-#         password_field = driver.find_element(By.XPATH, '//*[@id="password"]')
+#         password_field = driver.find_element(By.XPATH, '//*[@id="i0118"]')
 #         password_field.send_keys(password)
 
 #         # Press Enter to submit the form
 #         password_field.send_keys(Keys.RETURN)
 
-#         # Wait for the element to be clickable and then click it
-#         WebDriverWait(driver, 120).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="pt1:_UIScil1u"]'))).click()
-#         # Call the next function
+#         # # Wait for the element to be clickable and then click it
+#         # WebDriverWait(driver, 120).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="pt1:_UIScil1u"]'))).click()
+#         # # Call the next function
 #     finally:
 #         # Close the browser
 #         driver.quit()
-
--- src/main.py
+++ src/main.py
@@ -1,32 +1,27 @@
 import requests
 import webbrowser
-from msal import ConfidentialClientApplication, PublicClientApplication
+from msal import ConfidentialClientApplication
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import keyring
+import logging
+from selenium.common.exceptions import WebDriverException
 
 # Replace these variables with your actual values
-
 tenant_id = 'e220d369-e6a8-4126-83ef-b4b6bbfd9367'
 form_id = 'adMg4qjmJkGD77S2u_2TZz69AtZSZ0pLncDHq2Ag8tNUM1ZBQkFZVUFCMEYyNEM0SDJFNUhUTDZBUS4u'
 user_id = 'd602bd3e-6752-4b4a-9dc0-c7ab6020f2d3'
-
 client_id = '60390fd9-539d-4b94-b595-6b7d5ab57f48'
 client_secret = 'nxj8Q~l5Ym799LxBI0xIzpJ3LF4oY8mQRuXV4a7q'
 SCOPE = ["User.ReadBasic.All"]
 
-
-def forms_call():
-    # Fetch form responses
-    headers = {
-        'Authorization': f'Bearer {access_token}'
-    }
-    url = f"https://forms.office.com/formapi/DownloadExcelFile.ashx?formid={form_id}"
-    response2 = requests.get(url, headers=headers)
-    form_responses = response2.json()
-
-    print(form_responses)
-
-def token():
+def get_token():
     # Get access token
     url = f"https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/token"
+
     payload = {
         'grant_type': 'client_credentials',
         'client_id': client_id,
@@ -35,29 +30,26 @@ def token():
     }
     response = requests.post(url, data=payload)
     access_token = response.json().get('access_token')
-    print(access_token)
-    
+    return access_token
 
-client = ConfidentialClientApplication (client_id=client_id, client_credential=client_secret)
-authorization_url = client.get_authorization_request_url(SCOPE)
-print(authorization_url)
-webbrowser.open(authorization_url)
+def fetch_form_responses(access_token):
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    url = f"https://graph.microsoft.com/v1.0/users/{user_id}/drive/items/{form_id}/workbook/worksheets"
+    response = requests.get(url, headers=headers)
+    form_responses = response.json()
 
-def login():
-    # Technically we could use empty list [] as scopes to do just sign in,
-    # here we choose to also collect end user consent upfront
-    session = _build_auth_code_flow(scopes=SCOPE)
-    print(session)
-    forms_call()
-    # return render_template("login.html", auth_url=session["flow"]["auth_uri"], version=msal.__version__)
+def main():
+    # Get token and fetch form responses
+    access_token = get_token()
+    fetch_form_responses(access_token)
 
-def _build_msal_app(cache=None, authority=None):
-    return ConfidentialClientApplication(
-        client_id.CLIENT_ID, authority=authority or client_id.AUTHORITY,
-        client_credential=client_id.CLIENT_SECRET, token_cache=cache)
+    # Authorization URL for further actions if needed
 
-def _build_auth_code_flow(authority=None, scopes=None):
-    return _build_msal_app(authority=authority).initiate_auth_code_flow(
-        scopes or [],
-        redirect_uri=url_for("authorized", _external=True)) 
-token()        
+    client = ConfidentialClientApplication(client_id=client_id, client_credential=client_secret, authority=f"https://login.microsoftonline.com/{tenant_id}/")
+    authorization_url = client.get_authorization_request_url(SCOPE)
+    print(authorization_url)
+    webbrowser.open(authorization_url)
+    print("chegou aqui")
+main()
